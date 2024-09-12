df1126feb10bba01ed4f432441dd85572d4462c9
Carlos Rodriguez Asensio <carlosrasensio@gmail.com>
2024-09-13 00:47:07 +0200

    Create new visionOS Xcode project

---
+++ SpatialShoes/SpatialShoes/SpatialShoesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpatialShoesApp.swift
+//  SpatialShoes
+//
+//  Created by Carlos Rodriguez Asensio on 13/9/24.
+//
+
+import SwiftUI
+
+@main
+struct SpatialShoesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
