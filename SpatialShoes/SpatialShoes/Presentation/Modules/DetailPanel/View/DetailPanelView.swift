8b99b4660e2be1da5d55403f3c70c36c48e0baf8
Carlos Rodriguez Asensio <carlosrasensio@gmail.com>
2024-09-18 04:42:03 +0200

    Modify detail panel view. Add localizables

--- SpatialShoes/SpatialShoes/Presentation/Modules/DetailPanel/View/DetailPanelView.swift
+++ SpatialShoes/SpatialShoes/Presentation/Modules/DetailPanel/View/DetailPanelView.swift
@@ -17,21 +17,50 @@ struct DetailPanelView: View {
     
     var body: some View {
         Form {
-            LabeledContent("**Marca**", value: shoe.brand)
-            LabeledContent("**Tallas**", value: shoe.size.map { String($0) }.joined(separator: ", "))
-            LabeledContent("**Precio**", value: "\(shoe.price) €")
-            LabeledContent("**Descripción**", value: shoe.description)
-            LabeledContent("**Tipo**", value: shoe.type)
-            LabeledContent("**Materiales**", value: shoe.materials.joined(separator: ", "))
-            LabeledContent("**Origen**", value: shoe.origin)
-            LabeledContent("**Género**", value: shoe.gender)
-            LabeledContent("**Peso**", value: "\(shoe.weight) kg")
-            LabeledContent("**Colores**", value: shoe.colors.joined(separator: ", "))
-            LabeledContent("**Garantía**", value: "\(shoe.warranty) años")
-            LabeledContent("**Certificaciones**", value: shoe.certifications.joined(separator: ", "))
+            Section {
+                LabeledContent("**\(Localizables.brand)**", value: shoe.brand)
+                LabeledContent("**\(Localizables.size)**", value: shoe.size.map { String($0) }.joined(separator: ", "))
+                LabeledContent("**\(Localizables.price)**", value: "\(shoe.price) €")
+                LabeledContent("**\(Localizables.type)**", value: shoe.type)
+                LabeledContent("**\(Localizables.materials)**", value: shoe.materials.joined(separator: ", "))
+                LabeledContent("**\(Localizables.origin)**", value: shoe.origin)
+                LabeledContent("**\(Localizables.gender)**", value: shoe.gender)
+                LabeledContent("**\(Localizables.weight)**", value: "\(shoe.weight) kg")
+                LabeledContent("**\(Localizables.colors)**", value: shoe.colors.joined(separator: ", "))
+                LabeledContent("**\(Localizables.warranty)**", value: "\(shoe.warranty) años")
+                LabeledContent("**\(Localizables.certifications)**", value: shoe.certifications.joined(separator: ", "))
+            } header: {
+                Text("**Información**")
+            }
+            
+            Section {
+                Text(shoe.description)
+            } header: {
+                Text("**\(Localizables.description)**")
+            }
         }
         .navigationTitle(shoe.name)
         .background(Color.blue.opacity(0.4))
         .foregroundColor(.white)
+    }
+}
+
+// MARK: - Localizables
+
+private extension DetailPanelView {
+    enum Localizables {
+        static let description = "Descripción"
+        static let brand = "Marca"
+        static let size = "Tallas"
+        static let price = "Precio"
+        static let type = "Tipo"
+        static let materials = "Materiales"
+        static let origin = "Orígen"
+        static let gender = "Género"
+        static let weight = "Peso"
+        static let colors = "Colores"
+        static let warranty = "Garantía"
+        static let certifications = "Certificaciones"
+        static let info = "Información"
     }
 }
