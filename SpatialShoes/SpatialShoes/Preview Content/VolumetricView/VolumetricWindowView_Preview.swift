1075067112c08f6dd3ab668be43f9b2ddb1642a3
Carlos Rodriguez Asensio <carlosrasensio@gmail.com>
2024-09-20 03:25:58 +0200

    Create volumetric window view preview file

---
+++ SpatialShoes/SpatialShoes/Preview Content/VolumetricView/VolumetricWindowView_Preview.swift
@@ -0,0 +1,14 @@
+//
+//  VolumetricWindowView_Preview.swift
+//  SpatialShoes
+//
+//  Created by Carlos Rodriguez Asensio on 20/9/24.
+//
+
+import SwiftUI
+
+#Preview(windowStyle: .volumetric,
+         traits: .fixedLayout(width: 1.0, height: 1.0, depth: 1.0),
+         body: {
+    VolumetricWindowView()
+})
