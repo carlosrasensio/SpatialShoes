4dc9ec0c7471179612044e3ee28326fe3e868375
Carlos Rodriguez Asensio <carlosrasensio@gmail.com>
2024-09-21 02:54:32 +0200

    Extract reality panel view to extension file

--- SpatialShoes/SpatialShoes/Presentation/Modules/RealityPanel/View/RealityPanelView.swift
+++ SpatialShoes/SpatialShoes/Presentation/Modules/RealityPanel/View/RealityPanelView.swift
@@ -5,20 +5,18 @@
 //  Created by Carlos Rodriguez Asensio on 18/9/24.
 //
 
-import RealityKit
-import SpatialShoesScene
 import SwiftUI
 
 struct RealityPanelView: View {
     
     // MARK: - Public Properties
     
+    @Environment(\.openWindow) var openWindow
     @State var viewModel: RealityPanelViewModel
     let shoe: Shoe
     
     // MARK: - Private Properties
 
-    @Environment(\.openWindow) private var openWindow
     
     // MARK: - View
     
@@ -27,28 +25,7 @@ struct RealityPanelView: View {
             if viewModel.showLoader {
                 ProgressView(Localizables.loaderText)
             } else {
-                Model3D(named: shoe.model3DName, bundle: spatialShoesSceneBundle) { model in
-                    model
-                        .resizable()
-                        .scaledToFit()
-                        .scaleEffect(x: 0.3, y: 0.3, z: 0.3)
-                        .rotation3DEffect(.degrees(viewModel.rotationAngle),
-                                          axis: (x: 0, y: 1, z: 0))
-                } placeholder: {
-                    ProgressView("\(shoe.name) \(Localizables.loaderText.lowercased())")
-                }
-                
-                Button(action: {
-                    viewModel.isRotating.toggle()
-                }) {
-                    Text(viewModel.isRotating ? Localizables.disableExhibitorMode : Localizables.enableExhibitorMode)
-                }
-                
-                Button(Localizables.showVolumetricWindow) {
-                    openWindow(id: Constants.volumetricWindowID)
-                }
-                
-                Spacer()
+                createRealityPanel(with: shoe)
             }
         }
         .frame(maxWidth: .infinity)
@@ -95,9 +72,8 @@ private extension RealityPanelView {
 
 // MARK: - Constants
 
-private extension RealityPanelView {
+extension RealityPanelView {
     enum Constants {
-        static let volumetricWindowID = Global.Constants.shoeVolumetricWindowID
         
         // MARK: - Icons
         
